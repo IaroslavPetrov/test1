c54cf2de6622b6b50e0f6600da78f962aa7a5d98
aroslavpetrov <ya.a.petrov@gmail.com>
2022-01-30 22:22:53 +0300

    2 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        int a = 42;
+        int b = 38;
+        int middleAge = (a + b) / 2;
+        System.out.println( "Средний возраст равен " + middleAge);
+
+
     }
 }
